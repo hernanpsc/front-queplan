4b75e04c4e85fcf1aa1ad8387171f4620c66b34f
hernan527 <hernan527@gmail.com>
2023-06-05 15:50:04 -0300

    allow origin

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 import cors from "cors";
 import express from "express";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
 import { connectToDatabase } from "./conection/database";
 import { employeesRouter } from "./routes/employees.routes";
@@ -13,51 +13,49 @@ import { collections } from './conection/database';
 import { listasdepreciosRouter } from "./routes/listasdeprecios.routes";
 import { postsRouter } from "./routes/posts.routes";
 
-
-
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
 
-const { ATLAS_URI } = process.env;
+const { ATLAS_URI, PORT } = process.env;
+
+const whitelist = ['http://localhost:63744'];
 
 if (!ATLAS_URI) {
-    console.error("No ATLAS_URI environment variable has been defined in config.env");
-    process.exit(1);
+  console.error("No ATLAS_URI environment variable has been defined in config.env");
+  process.exit(1);
 }
 
 connectToDatabase(ATLAS_URI)
-    .then(() => {
-        const app = express();
-        app.get('/', (req, res) => {
-            res.send('Hello  World!');
-          });
-        app.use(cors());
-        app.use("/employees",employeesRouter);
-        
-        app.use("/empresas", empresasRouter);
-        app.use("/planes", planesRouter);
-        app.use("/clinicas", clinicasRouter);
-        app.use('/cotizacion', cotizacionRouter);
-        app.use('/precios', listasdepreciosRouter);
-        app.use('/posts', postsRouter);
+  .then(() => {
+    const app = express();
 
-       app.use(cors())
-       
-        const {PORT} = process.env
-        // start the Express server
-        app.listen(PORT, () => {
-            console.log(`Server running at http://localhost:` + PORT + `...`);
-        });
+    app.use(cors({
+      origin: whitelist
+    }));
 
-    })
-    .catch(error => console.error(error));
+    app.get('/', (req, res) => {
+      res.send('Hello World!');
+    });
 
-    export const  getEmployees2 = async (req: Request, res: Response) => {
-        try {
-          const  employees = await collections.employees.find({}).toArray();
-          res.status(200).send(employees);
-        } catch (error) {
-          res.status(500).send(error.message);
-        }
-      };
-      
+    app.use("/employees", employeesRouter);
+    app.use("/empresas", empresasRouter);
+    app.use("/planes", planesRouter);
+    app.use("/clinicas", clinicasRouter);
+    app.use('/cotizacion', cotizacionRouter);
+    app.use('/precios', listasdepreciosRouter);
+    app.use('/posts', postsRouter);
+
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:` + PORT + `...`);
+    });
+  })
+  .catch(error => console.error(error));
+
+export const getEmployees2 = async (req: Request, res: Response) => {
+  try {
+    const employees = await collections.employees.find({}).toArray();
+    res.status(200).send(employees);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
